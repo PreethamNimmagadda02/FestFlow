182bf9f54d043f83a5d3c4d15646deaba1de2b15
Preetham Nimmagadda <141296263+PreethamNimmagadda02@users.noreply.github.com>
2025-09-25 00:06:06 +0530

    Update firebase.ts

--- services/firebase.ts
+++ services/firebase.ts
@@ -11,7 +11,7 @@ const firebaseConfig = {
   storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.VITE_FIREBASE_APP_ID,
-  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID"
+  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase only if it hasn't been initialized yet.
