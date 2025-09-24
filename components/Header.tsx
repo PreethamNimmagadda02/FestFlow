4c7bca6dc549e9a2efe25145eb1d1761cad2b185
Preetham Nimmagadda <141296263+PreethamNimmagadda02@users.noreply.github.com>
2025-09-25 00:47:18 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,13 +7,7 @@ import { GoogleIcon } from './icons/GoogleIcon';
 import { CheckCircleIcon } from './icons/CheckCircleIcon';
 import { XCircleIcon } from './icons/XCircleIcon';
 import { TrashIcon } from './icons/TrashIcon';
-
-const ResetIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-    <polyline points="1 4 1 10 7 10"></polyline>
-    <path d="M3.51 15a9 9 0 1 0 2.13-9.36L1 10"></path>
-  </svg>
-);
+import { FilePlusIcon } from './icons/FilePlusIcon';
 
 interface HeaderProps {
     onResetClick: () => void;
@@ -68,7 +62,7 @@ export const Header: React.FC<HeaderProps> = React.memo(({ onResetClick, onDelet
                     title="Reset to a new, blank plan"
                     aria-label="New/Reset Plan"
                 >
-                    <ResetIcon className="w-4 h-4" />
+                    <FilePlusIcon className="w-4 h-4" />
                     <span>New / Reset</span>
                 </button>
                  <button
