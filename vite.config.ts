e90ff6679e5340981c98497910936960b7f20457
Preetham Nimmagadda <141296263+PreethamNimmagadda02@users.noreply.github.com>
2025-09-25 01:07:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const env = loadEnv(mode, process.cwd(), '');
     return {
       define: {
-        'process.env.API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
+        'process.env': env
       },
       resolve: {
         alias: {
